3e23f51482af053673d43a5469fe92b24bce0f92
jfam290674 <jfavila@nexosecuador.site>
2025-01-10 14:33:54 -0500

    Corregir /about y mejorar diseño de home.html

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from app import app
 
+# Ruta principal
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# Nueva ruta: About
 @app.route('/about')
 def about():
     return render_template('about.html')
