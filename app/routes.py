23a66304b706fb677384a72ec1f6474a49c390b6
jfam290674 <jfavila@nexosecuador.site>
2025-01-10 14:27:48 -0500

    Configurar TailwindCSS y crear plantillas definitivas para Nexos Ecuador

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,10 @@
 from flask import render_template
 from app import app
 
-# Ruta principal
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# Nueva ruta: About
 @app.route('/about')
 def about():
-    return "Esta es la página de 'Acerca de'. Aquí podrías describir tu aplicación."
+    return render_template('about.html')
