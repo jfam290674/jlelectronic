57442d19cd8e3d55400e38f491891c0b47c7039f
jfam290674 <jfavila@nexosecuador.site>
2025-01-10 14:07:57 -0500

    Agregar nueva ruta /about

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
 from app import app
 
+# Ruta principal
 @app.route('/')
 def home():
     return "¡Hola desde Visual Studio Code! Este cambio se refleja en GitHub y cPanel."
+
+# Nueva ruta: About
+@app.route('/about')
+def about():
+    return "Esta es la página de 'Acerca de'. Aquí podrías describir tu aplicación."
