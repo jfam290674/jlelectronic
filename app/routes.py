13cef33b3f82e8b291fa6773a68269caf231d362
jfam290674 <jfavila@nexosecuador.site>
2025-01-10 14:02:50 -0500

    Actualizar mensaje en la ruta / desde VS Code

--- app/routes.py
+++ app/routes.py
@@ -2,4 +2,4 @@ from app import app
 
 @app.route('/')
 def home():
-    return "¡Hola, mundo! Esta es una aplicación Flask modular."
+    return "¡Hola desde Visual Studio Code! Este cambio se refleja en GitHub y cPanel."
